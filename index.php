b71bf193e4be4df2f25fcab8c1df85c8c60b7e8f
Jian Jaico Cajita <57510953+jianjaico-code@users.noreply.github.com>
2021-12-21 18:02:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                 aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href="https://www.linkedin.com/in/jianjaico/"
                 target="_blank">
-                <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+<!--                 <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo"> -->
                 <span class="ms-1 font-weight-bold text-white">Ordering System</span>
             </a>
         </div>
